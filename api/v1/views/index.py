9d6f932fa9144d72d337fcbf160e5179dec5e071
GreatBull22 <brandonmpofu22@gmail.com>
2024-03-17 07:46:32 -0700

    AirBnB_clone_v3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,16 +7,19 @@ from flask import jsonify
 
 @app_views.route('/status', strict_slashes=False)
 def status():
-    """ route that returns a JSON status"""
+    """Returns a JSON object with status 'OK'.""" 
     return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats', strict_slashes=False)
 def stats():
-    """ return object count"""
-    return jsonify({"amenities": storage.count("Amenity"),
-                    "cities": storage.count("City"),
-                    "places": storage.count("Place"),
-                    "reviews": storage.count("Review"),
-                    "states": storage.count("State"),
-                    "users": storage.count("User")})
+    """Returns a JSON object containing the number of each object type.""" 
+    return jsonify({
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    })
+
